d4443f509d99004ab1c46b18c3bc3e9d9215b468
pavanjoshi421 <pavanjoshi397@gmail.com>
2022-04-24 20:07:42 +0530

    commit by pavan1

--- src/test/java/com/crm/PRACTICE/Dataproviderpractice1.java
+++ src/test/java/com/crm/PRACTICE/Dataproviderpractice1.java
@@ -19,17 +19,20 @@ Object[][] obj=new Object[6][2];
 		obj[1][0]="kawasaki";
 		obj[1][1]=200;
 		
-		obj[2][0]="Rx100";
+		obj[2][0]="Rx200";
 		obj[2][1]=100;
 		
-		obj[3][0]="Hero";
+		obj[3][0]="HeroHonda";
 		obj[3][1]=150;
 		
 		obj[4][0]="Royal Enfield Thunder";
 		obj[4][1]=300;
 		
-		obj[5][0]="splender";
+		obj[5][0]="splenderxxx";
 		obj[5][1]=120;
+		
+		obj[5][0]="splender";
+		obj[5][1]=130;
 		
 		return obj;
 		
